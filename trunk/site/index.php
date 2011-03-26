fb9c82cd4c828bc896d250fb25ad2c5579c9d346
nmauchle@gmail.com <nmauchle@gmail.com@821ca84f-ac6e-b6e1-18a7-74bf5fc2098e>
2011-03-26 17:08:56 +0000

    Get image from database

--- trunk/site/index.php
+++ trunk/site/index.php
@@ -6,6 +6,7 @@ echo '<head>
   <style></style>
 </head>';
 
+echo 'Kategorien: ';
 $query = "select id, name from Category";
 $result = mysql_query($query);
 while( $row = mysql_fetch_array($result) ) {
@@ -16,3 +17,20 @@ echo '<table>
        </tr>
      </table>';
  }
+
+echo 'Bilder in der Datenbank:';
+$query = "select i.id, i.name, i.pfad, i.date, c.name as category from Images i, Category c where i.Category_id = c.id";
+$result = mysql_query($query);
+while( $row = mysql_fetch_array($result) ) {
+echo '<table>
+       <tr>
+         <td align="left">'.$row['id'].' </td>
+         <td align="left">'.$row['name'].' </td>
+         <td align="left">'.$row['pfad'].' </td>
+         <td align="left">'.$row['date'].' </td>
+         <td align="left">'.$row['category'].' </td>
+       </tr>
+     </table>';
+echo "Zeige Bild an: <br /> <img src='..".$row['pfad']."'/>";
+ }
+

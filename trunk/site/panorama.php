057ed9c51ece9a5f050eecda2095b389300f8758
nmauchle@gmail.com <nmauchle@gmail.com@821ca84f-ac6e-b6e1-18a7-74bf5fc2098e>
2011-04-03 09:10:24 +0000

    Add panorama function

---
+++ trunk/site/panorama.php
@@ -0,0 +1,32 @@
+<?php
+header ("Content-type: image/jpg");
+
+$images = array();
+$images[0] = imagecreatefromjpeg('../images/1.jpg');
+$images[1] = imagecreatefromjpeg('../images/1.jpg');
+$images[2] = imagecreatefromjpeg('../images/1.jpg');
+
+$sizes = array();
+$sizes[0] = 0;
+$sizes[1] = 640;
+$sizes[2] = 1280;
+
+
+$new_image_width = imagesx($images[0]) *3;
+$new_image_height = imagesy($images[0]);
+
+
+$new_image = imagecreatetruecolor($new_image_width, $new_image_height);
+
+for($i=0; $i<3; $i++) {
+  imagecopymerge($new_image, $images[$i], $sizes[$i], 0, 0, 0, $new_image_width, $new_image_height ,100);
+}
+//imagejpeg($new_image);
+imagejpeg($new_image, '/var/www/mountaincam/images/test.jpg', 100);
+
+
+foreach($images as $image) {
+  imagedestroy($image);
+}
+//imagedestroy($new_image);
+?>

c4edbab9a5d47f14bf210ba1f94469ce170ef911
nmauchle@gmail.com <nmauchle@gmail.com@821ca84f-ac6e-b6e1-18a7-74bf5fc2098e>
2011-05-24 12:13:02 +0000

    final release

--- trunk/site/inc/config.php
+++ trunk/site/inc/config.php
@@ -1,9 +1,12 @@
 <?php
-        $camAddress ='http://root:gibbiX12345@192.168.10.137';
-//	$camAddress = 'http://192.168.10.137';
+// Addresse der Kamera
+        $camAddress ='http://root:gibbiX12345@192.168.10.134';
+//Falls die Webapplikation auf einem Windows-Rechner läuft muss man die Pfade
+//richtig anpassen
 	if(PHP_OS == "WIN32" || PHP_OS == "WINNT"){
 		$imagepath = "C:\\xampp\htdocs\mountaincam\images\\";
 	}
+//Default Pfad ist /var/www/mountaincam/images
 	else{
 		$imagepath = "/var/www/mountaincam/images/";
 	}
